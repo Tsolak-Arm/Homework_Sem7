02a1af0a139d08b84e8be32fd1ea88753d15efa0
Tsolak <dcntsolak@yahoo.com>
2024-02-02 12:28:53 +0400

    Added Homework_Sem7:Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 1: Задайте значения M и N.
+// Напишите программу, которая выведет
+// все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+// Пример: M = 1; N = 5 -> "1, 2, 3, 4, 5"
+//        M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+
+  using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Натуральные числа в промежутке от {m} до {n}: ");
+        PrintNaturalNumbers(m, n);
+
+        Console.ReadLine();
+    }
+
+    static void PrintNaturalNumbers(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.Write(m);
+
+            if (m < n)
+            {
+                Console.Write(", ");
+            }
+
+            PrintNaturalNumbers(m + 1, n);
+        }
+    }
+}
+  
