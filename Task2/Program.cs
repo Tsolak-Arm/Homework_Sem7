cb556ab60d15ec506ef5667634459bddc78a3aa4
Tsolak <dcntsolak@yahoo.com>
2024-02-03 14:23:01 +0400

    Solved Homework_Sem7:Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,5 +1,30 @@
 ﻿// Задача 2:
 // Напишите программу вычисления
 // функции Аккермана с помощью рекурсии.
-// Даны два неотрицательных числа m и n. 
+// Даны два неотрицательных числа m и n.
+
+int SolveAkkermanFunction(int m, int n)
+{
+    int result = 0;
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return SolveAkkermanFunction(m - 1, 1);
+    }
+    else if (m > 0 && n > 0)
+    {
+        return SolveAkkermanFunction(m - 1, SolveAkkermanFunction(m, n - 1));
+    }
+    return result;
+}
+
+Console.WriteLine($"Input m: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Input n: ");
+int n = int.Parse(Console.ReadLine());
+int result = SolveAkkermanFunction(m, n);
+Console.WriteLine($"A({m}, {n}) = {result}");
 
--- Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+982052c6dcd6b2eb018f1988d6056cf5075c2863")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e0325cb9cfd904efdee09cc56f1b1900e7eb709")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
