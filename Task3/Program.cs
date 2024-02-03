9748b1a4b6ff8464a78b1e9194e6a4766a2112f5
Tsolak <dcntsolak@yahoo.com>
2024-02-03 17:12:00 +0400

    Added_Homework_Sem7:Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 3: Задайте произвольный массив.
+// Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы.
+// Пример: [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
+
+
+int[] arr = { 15, 7, 36, 99, 103, 2, 27 };
+
+    void CreateRevArray(int[] arr, int i)
+    {
+        if (i >= 0)
+        {
+            Console.Write($"{arr[i]} ");
+            CreateRevArray(arr, i - 1);
+        }
+    }
+
+Console.Write("Reversed Array: ");
+CreateRevArray(arr, arr.Length - 1);
+
+Console.ReadLine();
+
+
